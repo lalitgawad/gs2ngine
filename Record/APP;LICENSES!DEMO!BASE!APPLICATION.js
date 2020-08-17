9d94dfd034123d23530a5a2602ed0d88337dfbb0
SShiralikar <Accelainc1@>
2020-08-17 17:55:30 -0400

    Inspection, POC creation and WF movement changes.

--- Record/APP;LICENSES!DEMO!BASE!APPLICATION.js
+++ Record/APP;LICENSES!DEMO!BASE!APPLICATION.js
@@ -3,7 +3,7 @@
  | Trigger : N/A
  | Event : N/A
  | Usage : Application Object use in events to implement city buiseness
- | Agency : 
+ | Agency :
  | Purpose : Application Object
  | Mark  : New Script
  | Notes : Initial Version
@@ -72,7 +72,7 @@ function APP_OBJ(identity, caller) {
             gs2.common.handleError(err, "");
         }
     }
-    
+
 	/**
      * Initialize
      */
@@ -84,7 +84,7 @@ function APP_OBJ(identity, caller) {
         this.initGlobalObjects();
         logDebug("INIT BASE");
     }
-    
+
 	//Initialize
     this.Init();
 
@@ -208,7 +208,7 @@ function APP_OBJ(identity, caller) {
 
     }
 
-    
+
     this.AsbDelegator = function () {
 
     }
@@ -217,7 +217,11 @@ function APP_OBJ(identity, caller) {
      * CTRCA Delegator to call local function(s) for record specific after logic
      */
     this.CtrcaDelegator = function () {
-        gs2.common.closeWfTask(capId, "Application Intake", "Intake Complete", "Intake Complete", "");
+        if(balanceDue > 0)
+        {
+            gs2.common.closeWfTask(capId, "Application Intake", "Payment Complete", "Payment Complete", "");
+            gs2.common.closeWfTask(capId, "Application Intake", "Intake Complete", "Intake Complete", "");
+        }
     }
 
     this.ISHBDelegator = function () {
@@ -225,22 +229,22 @@ function APP_OBJ(identity, caller) {
     }
     this.ISADelegator = function()
     {
-        
+
     }
     /**
      * IRSB Delegator to call local function(s) for record specific after logic
      */
     this.IRSBDelegator = function () {
-        
+
     }
     /**
      * IRMB Delegator to call local function(s) for record specific after logic
      */
     this.IRMBDelegator = function () {
-        
+
     }
     this.ISHADelegator = function () {
-        
+
     }
     this.IRSADelegator = function () {
         if(inspResult == "Compliant - Finalized")
@@ -274,7 +278,7 @@ function APP_OBJ(identity, caller) {
      * ASA Async Delegator to call local function(s) for record specific after logic
      */
     this.AsaAsyncDelegator = function () {
-
+        //gs2.common.closeWfTask(capId, "Application Intake", "Intake Complete", "Intake Complete", "");
     }
 
     /**
@@ -287,7 +291,7 @@ function APP_OBJ(identity, caller) {
     /**
      * Workflow task update after Delegator to call local function(s) for record specific after logic
      */
-    this.WtuaDelegator = function ()
+    this.WtuaDelegator = function()
     {
         if(wfTask == "Application Review" && wfStatus == "Application Approved - Inspection Needed")
         {
@@ -302,6 +306,24 @@ function APP_OBJ(identity, caller) {
             editAppName("",pocCapId);
             var pocItemsArr = this.getPOCItems();
             addASITable("DIFICIENCY LISTING", pocItemsArr, pocCapId);
+            gs2.wf.deActivateWfTask(capId, "Supervisory Review");
+        }
+        else if(wfTask == "Supervisory Review" && wfStatus == "Review Complete")
+        {
+            var licCapId = gs2.rec.createParent(appTypeArray[0],appTypeArray[1],appTypeArray[2],"License");
+            gs2.rec.updateAppStatus("Active","", licCapId);
+            editAppName("",pocCapId);
+        }
+        else if(wfTask == "Application Review" && wfStatus == "Additional Information Required")
+        {
+            gs2.wf.deActivateWfTask(capId, "Application Review");
+            //gs2.rec.addStdConditionWithComments("Licensing", "Addtional Information Required", "Additional Information Required","Additional Information Required" , wfComment , null);
+            addSTDConditionX("Addtional Information Required", "Additional Information Required", capId);
+        }
+        else if(wfTask == "Application Review" && wfStatus == "Additional Information Received")
+        {
+            gs2.wf.activateTask(capId, "Application Review");
+            editCapConditionStatus("Additional Information Required","Additional Information Required","Condition Met","Not Applied")
         }
     }
 
@@ -317,7 +339,7 @@ function APP_OBJ(identity, caller) {
         return pbValid;
     }
 
-    
+
 	this.pageflowDelegator = function (pfName) {
         var hidepage = true;
 
@@ -361,5 +383,106 @@ function APP_OBJ(identity, caller) {
             }
         }
         return vArr;
+    }
+}
+function moveWFTask(ipTask, ipStatus, ipComment, ipNote) // Optional CapID, Process, StatusDate
+{
+    var vCapId = capId;
+    if (arguments.length > 4 && arguments[4] != null) {
+        var vCapId = arguments[4];
+    }
+
+    if (ipTask == "")
+        ipTask = getCurrentTask(vCapId).getTaskDescription();
+
+    var vUseProcess = false;
+    var vProcessName = "";
+    if (arguments.length > 5 && arguments[5] != null && arguments[5] != "") {
+        vProcessName = arguments[5]; // subprocess
+        vUseProcess = true;
+    }
+
+    var vUseStatusDate = false;
+    var vStatusDate = null;
+    var vToday = new Date();
+    if (arguments.length > 6 && arguments[6] != null && arguments[6] != "") {
+        vStatusDate = new Date(arguments[6]);
+        vUseStatusDate = true;
+    }
+
+    var vWFResult = aa.workflow.getTaskItems(vCapId, ipTask, vProcessName, null, null, null);
+    if (vWFResult.getSuccess())
+        var vWFObj = vWFResult.getOutput();
+    else {
+        logMessage("**ERROR: Failed to get workflow object: " + vWFResult.getErrorMessage());
+        return false;
+    }
+
+    if (!ipStatus)
+        ipStatus = "NA";
+
+    if (vWFObj.length == 0)
+        return false;
+
+    var vMoved = false;
+    for (var vCounter in vWFObj) {
+        var vTaskObj = vWFObj[vCounter];
+        if (vTaskObj.getTaskDescription().toUpperCase().equals(ipTask.toUpperCase()) && (!vUseProcess || vTaskObj.getProcessCode().equals(vProcessName))) {
+            var vTaskStatusObj = aa.workflow.getTaskStatus(vTaskObj, ipStatus).getOutput();
+            if (!vTaskStatusObj)
+                continue;
+            if (vUseStatusDate) {
+                var vTaskModel = vTaskObj.getTaskItem();
+                vTaskModel.setStatusDate(vStatusDate);
+                vTaskModel.setDisposition(ipStatus);
+                vTaskModel.setDispositionNote(ipNote);
+                vTaskModel.setDispositionComment(ipComment);
+                vTaskModel.setDispositionDate(vToday);
+                aa.workflow.handleDisposition(vTaskModel, vCapId);
+                vMoved = true;
+                logMessage("Moved Workflow Task: " + ipTask + " with status " + ipStatus);
+                logDebug("Moved Workflow Task: " + ipTask + " with status " + ipStatus);
+            } else {
+                var vResultAction = vTaskStatusObj.resultAction;
+                var vStepNumber = vTaskObj.getStepNumber();
+                var vProcessID = vTaskObj.getProcessID();
+                var vDispositionDate = aa.date.getCurrentDate();
+
+                if (vUseProcess)
+                    aa.workflow.handleDisposition(vCapId, vStepNumber, vProcessID, ipStatus, vDispositionDate, ipNote, ipComment, systemUserObj, vResultAction);
+                else
+                    aa.workflow.handleDisposition(vCapId, vStepNumber, ipStatus, vDispositionDate, ipNote, ipComment, systemUserObj, vResultAction);
+
+                vMoved = true;
+                aa.print("Moved Workflow Task: " + ipTask + " with status " + ipStatus);
+                logDebug("Moved Workflow Task: " + ipTask + " with status " + ipStatus);
+            }
+        }
+    }
+    return vMoved;
+}
+//Add standard condition to the specific CapID
+function addSTDConditionX(cType, cDesc, vCapID) {
+
+    if (!aa.capCondition.getStandardConditions) {
+        logDebug("addStdCondition function is not available in this version of Accela Automation.");
+    }
+    else {
+        standardConditions = aa.capCondition.getStandardConditions(cType, cDesc).getOutput();
+        logDebug(standardConditions.length);
+        for (i = 0; i < standardConditions.length; i++) {
+            standardCondition = standardConditions[i];
+            if (standardCondition.getConditionType().toUpperCase() == cType.toUpperCase() && standardCondition.getConditionDesc().toUpperCase() == cDesc.toUpperCase()) {
+                standardCondition.setLongDescripton(wfComment);
+                var addCapCondResult = aa.capCondition.createCapConditionFromStdCondition(vCapID, standardCondition);
+
+                if (addCapCondResult.getSuccess()) {
+                    logDebug("Successfully added condition (" + standardCondition.getConditionDesc() + ")");
+                }
+                else {
+                    logDebug("**ERROR: adding condition (" + standardCondition.getConditionDesc() + "): " + addCapCondResult.getErrorMessage());
+                }
+            }
+        }
     }
 }
